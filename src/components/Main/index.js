552d9c4d0d80c5cb710cc9096c2c8a13b4a8c36d
Allef Gomes <allefgalmeida@gmail.com>
2020-04-11 02:03:05 -0300

    Initialize Main Component

---
+++ src/components/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Content = styled.main`
+  margin-left: 20%;
+  padding: 0px 10px;
+`
+
+const Main = () => (
+  <Content>
+    <h1>Experience</h1>
+  </Content>
+)
+
+export default Main;
