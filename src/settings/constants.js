d6c8c9db269a991312e750e7052fc1ae1722f003
Allef Gomes <allefgalmeida@gmail.com>
2020-04-11 02:00:53 -0300

    created AVATAR_URL and SKILLS constants

---
+++ src/settings/constants.js
@@ -0,0 +1,13 @@
+const AVATAR_URL = "https://avatars3.githubusercontent.com/u/25732016?s=460&u=fe2ce81f343feb14eb872528528fdd9b03dbd662&v=4"
+
+const SKILLS = [
+  { key: 'ror', name: 'Ruby on Rails', years: 3, percent: 90 },
+  { key: 'postgres', name: 'Postgres', years: 3, percent: 80 },
+  { key: 'react', name: 'React', years: 3, percent: 70 },
+  { key: 'vue', name: 'Elixir', years: 0.5, percent: 50 },
+]
+
+export {
+  AVATAR_URL,
+  SKILLS
+}
